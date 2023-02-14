72403cae801a6cd5c6f6e584ea71a7942fc923cb
howard <makubex49@gmail.com>
2023-02-14 17:11:44 +0800

    會動囉

--- test/linux/delta_io/delta_io.c
+++ test/linux/delta_io/delta_io.c
@@ -19,6 +19,8 @@
 
 #define EC_TIMEOUTMON 500
 
+#define ETH_CH_NAME "eno1"
+
 char IOmap[4096];
 OSAL_THREAD_HANDLE thread1;
 int expectedWKC;
@@ -26,6 +28,48 @@ boolean needlf;
 volatile int wkc;
 boolean inOP;
 uint8 currentgroup = 0;
+
+static int drive_write8(uint16 slave, uint16 index, uint8 subindex, uint8 value)
+{
+   int wkc;
+   wkc = ec_SDOwrite(slave, index, subindex, FALSE, sizeof(value), &value, EC_TIMEOUTRXM);
+   return wkc;
+}
+
+int delta_io_setup(ecx_contextt *context, uint16 slave)
+{
+   int wkc = 0;  
+   printf("[slave:%d %ld] DELTA RC-EC0902 setup\r\n", slave, (int64)context);
+
+   // Index: 2001 Datatype: 002a Objectcode: 09 Name: Active DO Enable
+   // Sub: 00 Datatype: 0005 Bitlength: 0008 Obj.access: 0007 Name: SubIndex 000
+   //          Value :0x04 4
+   // Sub: 01 Datatype: 0005 Bitlength: 0008 Obj.access: 003f Name: Active Port2 DO CH0~7 Enable
+   //          Value :0x00 0
+   // Sub: 02 Datatype: 0005 Bitlength: 0008 Obj.access: 003f Name: Active Port2 DO CH8~15 Enable
+   //          Value :0x00 0
+   // Sub: 03 Datatype: 0005 Bitlength: 0008 Obj.access: 003f Name: Active Port3 DO CH0~7 Enable
+   //          Value :0x00 0
+   // Sub: 04 Datatype: 0005 Bitlength: 0008 Obj.access: 003f Name: Active Port3 DO CH8~15 Enable
+   //          Value :0x00 0
+   wkc += drive_write8(slave, 0x2001, 1, 0xFF);
+   wkc += drive_write8(slave, 0x2001, 2, 0xFF);
+   wkc += drive_write8(slave, 0x2001, 3, 0xFF);
+   wkc += drive_write8(slave, 0x2001, 4, 0xFF);
+
+   //strncpy(ec_slave[slave].name, "IO", EC_MAXNAME);
+
+   if (wkc != 4)
+   {
+      printf("[slave:%d] setup failed\nwkc: %d\n", slave, wkc);
+      return -1;
+   }
+   else
+   {
+      printf("[slave:%d] setup succeed.\n", slave);
+      return 0;
+   }
+}
 
 void simpletest(char *ifname)
 {
@@ -37,18 +81,28 @@ void simpletest(char *ifname)
 
    printf("Starting simple test\n");
 
+
    /* initialise SOEM, bind socket to ifname */
    if (ec_init(ifname))
    {
       printf("ec_init on %s succeeded.\n", ifname);
+
+      /* request INIT state for all slaves */
+      ec_writestate(0);
+
       /* find and auto-config slaves */
 
       if (ec_config_init(FALSE) > 0)
       {
          printf("%d slaves found and configured.\n", ec_slavecount);
 
-         ec_config_map(&IOmap);
+         
+         ec_slave[0].PO2SOconfigx = delta_io_setup;
+         
+         uint8 value = 0xFF;
+         ec_SDOwrite(1, 0x2001, 1, FALSE, sizeof(value), &value, EC_TIMEOUTRXM);
 
+         ec_config_map(&IOmap);         
          ec_configdc();
 
          printf("Slaves mapped, state to SAFE_OP.\n");
@@ -90,8 +144,31 @@ void simpletest(char *ifname)
             printf("Operational state reached for all slaves.\n");
             inOP = TRUE;
             /* cyclic loop */
-            for (i = 1; i <= 10000; i++)
+            for (i = 1; i <= 1000; i++)
             {
+
+               static int blink = 0;
+               if ((clock_ms() - start_time) >= 1000)
+               {
+                  // printf("[%ld]1 second has passed.\r\n", clock_ms());
+                  blink = !blink;
+
+                  for (j = 0; j < oloop; j++)
+                  {
+                     //*(ec_slave[0].outputs + j) = blink ? 0xFF : 0x00;
+                     ec_slave[0].outputs[j] = blink ? 0xFF : 0x00;
+                     
+                  }
+                  //*ec_slave[0].outputs = blink ? 0xFF : 0x00;
+
+                  //uint8 outputs = blink ? 0xFF : 0x00;
+                  //ec_SDOwrite(0, 0x6200, 0x01, FALSE, sizeof(uint8), &outputs, EC_TIMEOUTRXM);
+                  
+
+                  start_time = clock_ms();
+
+               }
+
                ec_send_processdata();
                wkc = ec_receive_processdata(EC_TIMEOUTRET);
 
@@ -114,15 +191,6 @@ void simpletest(char *ifname)
                }
                osal_usleep(5000);
 
-               static int blink = 0;
-               if ((clock_ms() - start_time) >= 1000)
-               {
-                  // printf("[%ld]1 second has passed.\r\n", clock_ms());
-                  blink = !blink;
-
-                  *ec_slave[0].outputs = blink ? 0xFF : 0x00;
-                  start_time = clock_ms();
-               }
             }
             inOP = FALSE;
          }
@@ -235,22 +303,16 @@ OSAL_THREAD_FUNC ecatcheck(void *ptr)
    }
 }
 
-int main(int argc, char *argv[])
+//int main(int argc, char *argv[])
+int main(void)
 {
-   printf("SOEM (Simple Open EtherCAT Master)\nSimple test\n");
 
-   if (argc > 1)
-   {
-      /* create thread to handle slave error handling in OP */
-      //      pthread_create( &thread1, NULL, (void *) &ecatcheck, (void*) &ctime);
-      osal_thread_create(&thread1, 128000, &ecatcheck, (void *)&ctime);
-      /* start cyclic part */
-      simpletest(argv[1]);
-   }
-   else
-   {
-      printf("Usage: simple_test ifname1\nifname = eth0 for example\n");
-   }
+   printf("SOEM (Simple Open EtherCAT Master)\nSimple test\n");
+   /* create thread to handle slave error handling in OP */
+   //      pthread_create( &thread1, NULL, (void *) &ecatcheck, (void*) &ctime);
+   osal_thread_create(&thread1, 128000, &ecatcheck, (void *)&ctime);
+   /* start cyclic part */
+   simpletest(ETH_CH_NAME);
 
    printf("End program\n");
    return (0);
