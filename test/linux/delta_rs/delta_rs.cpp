40faa337108a9a2e98560c6ef54b642abbcf41bf
HowardWhile <makubex49gmail.com>
2023-06-14 15:44:19 +0800

    可以編譯c++ 開始設計

---
+++ test/linux/delta_rs/delta_rs.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <iostream>
+
+#include "arc_console.hpp"
+
+
+
+int main() 
+{   
+    console("SOEM (Simple Open EtherCAT Master)");
+    console("delta_rs start");
+
+    //
+}
