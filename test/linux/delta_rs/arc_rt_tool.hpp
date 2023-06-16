998eb70b9250086d1cbfc8a2f4b136e1eaeaed17
HowardWhile <makubex49gmail.com>
2023-06-16 09:03:07 +0800

    增加可以指定執行序優先權的函數 顯示rt性能

--- test/linux/delta_rs/arc_rt_tool.hpp
+++ test/linux/delta_rs/arc_rt_tool.hpp
@@ -17,11 +17,11 @@
 
 /**
  * @brief set_latency_target
- * 
+ *
  * 消除系統時間偏移的函數
  * 取自cyclic_test
  * https://github.com/jlelli/rt-tests/blob/master/src/cyclictest/cyclictest.c
- * 
+ *
  * 如果檔案 /dev/cpu_dma_latency 存在，
  * 開啟它並將零寫入其中。這將告訴電源管理系統不要轉換到高 cstate
  * （事實上，系統的行為就像是 idle=poll）。
@@ -35,20 +35,23 @@ static int set_latency_target(void)
     struct stat s;
     int ret;
 
-    if (stat("/dev/cpu_dma_latency", &s) != 0) {
+    if (stat("/dev/cpu_dma_latency", &s) != 0)
+    {
         console("# /dev/cpu_dma_latency does not exist.");
         return -1;
     }
 
     int latency_target_fd = open("/dev/cpu_dma_latency", O_RDWR);
-    if (latency_target_fd == -1) {
+    if (latency_target_fd == -1)
+    {
         console("# Failed to open /dev/cpu_dma_latency: %s", strerror(errno));
         return -1;
     }
 
     int32_t latency_target_value = 0;
     ret = write(latency_target_fd, &latency_target_value, sizeof(latency_target_value));
-    if (ret == 0) {
+    if (ret == 0)
+    {
         console("# error setting cpu_dma_latency to %d!: %s", latency_target_value, strerror(errno));
         close(latency_target_fd);
         return -1;
@@ -60,11 +63,10 @@ static int set_latency_target(void)
     return 0;
 }
 
-
 /**
  * @brief 將當前執行緒綁定到指定的 CPU
  *
- * @param cpu_id 目標 CPU 的 ID，指定執行緒運行的 CPU
+ * @param cpu_id 目標 CPU 的 ID，指定執行緒運行的 CPU，4核心對應ID 0~3
  * @return 成功時返回 0，失敗時返回非零錯誤碼
  */
 static int setThreadAffinity(int cpu_id)
@@ -75,10 +77,10 @@ static int setThreadAffinity(int cpu_id)
 
     pthread_t thread = pthread_self();
     int ret = pthread_setaffinity_np(thread, sizeof(mask), &mask);
-    if (ret != 0) {
+    if (ret != 0)
+    {
         // 設置 CPU Affinity 失敗
-        perror("pthread_setaffinity_np");
-        return ret;
+        console(RED "setThreadAffinity(%d) cpu_id = %d %s" RESET, ret, cpu_id, strerror(errno));
     }
 
     return ret;
@@ -87,23 +89,81 @@ static int setThreadAffinity(int cpu_id)
 /**
  * @brief 設置執行緒的優先權
  *
- * @param Priority 優先權值，較高的值表示較高的優先級
+ * @param Priority 優先權值，較高的值表示較高的優先級 99 = RT
  * @return 成功時返回 0，失敗時返回非零錯誤碼
  */
-static int setThreadPriority(int Priority)
+static int setThreadPriority(int priority)
 {
     pthread_t thread = pthread_self();
     struct sched_param schedp;
-    schedp.sched_priority = Priority;
+    schedp.sched_priority = priority;
 
     int ret = pthread_setschedparam(thread, SCHED_FIFO, &schedp);
-    if (ret != 0) {
+    if (ret != 0)
+    {
         // 設置優先權失敗
-        perror("pthread_setschedparam");
+        console(RED "setThreadPriority = %d %s" RESET, priority, strerror(errno));
         return ret;
     }
 
     return ret;
+}
+
+/**
+ * @brief 設置進程的優先級（Niceness）
+ *
+ * @param[in] niceness 優先級值，較小的值表示較高的優先級，範圍是介於 -20 到 +19 之間
+ * @return 成功時返回 0，失敗時返回非零錯誤碼
+ */
+int setThreadNiceness(int niceness)
+{
+    int ret = 0;
+    pid_t pid = getpid(); // 獲取進程 PID
+
+    ret = setpriority(PRIO_PROCESS, pid, niceness);
+    if (ret != 0)
+    {
+        console(RED "setThreadNiceness = (%d) %s" RESET, niceness, strerror(errno));
+    }
+
+    return ret;
+}
+
+/**
+ * @brief 計算兩個 timespec 結構體之間的時間差值（以納秒為單位）
+ *
+ * @param t1 第一個 timespec 結構體
+ * @param t2 第二個 timespec 結構體
+ * @return int64_t 兩個 timespec 結構體之間的時間差值（以納秒為單位）
+ */
+static int64_t calcDiff_ns(struct timespec t1, struct timespec t2)
+{
+    int64_t tdiff;
+    tdiff = NSEC_PER_SEC * (int64_t)((int)t1.tv_sec - (int)t2.tv_sec); // 計算秒數差值（乘以每秒的納秒數）
+    tdiff += ((int)t1.tv_nsec - (int)t2.tv_nsec);                      // 加上納秒數差值
+    return tdiff;
+}
+
+/**
+ * @brief 將指定的時間（以納秒為單位）添加到 timespec 結構體中
+ *
+ * @param ts 指向 timespec 結構體的指針
+ * @param addtime 要添加的時間（以納秒為單位）
+ */
+void addTimespec(struct timespec *ts, int64_t addtime)
+{
+    int64_t sec, nsec;
+
+    nsec = addtime % NSEC_PER_SEC;         // 取得納秒數的差值
+    sec = (addtime - nsec) / NSEC_PER_SEC; // 取得秒數的差值
+    ts->tv_sec += sec;                     // 添加秒數差值到 timespec 結構體的 tv_sec 成員
+    ts->tv_nsec += nsec;                   // 添加納秒數差值到 timespec 結構體的 tv_nsec 成員
+    if (ts->tv_nsec > NSEC_PER_SEC)
+    {
+        nsec = ts->tv_nsec % NSEC_PER_SEC;                 // 取得多餘的納秒數
+        ts->tv_sec += (ts->tv_nsec - nsec) / NSEC_PER_SEC; // 將多餘的納秒數轉換為秒數並添加到 tv_sec
+        ts->tv_nsec = nsec;                                // 將剩餘的納秒數存回 tv_nsec
+    }
 }
 
 #endif // rt_tool_h__
--- test/linux/delta_rs/delta_rs.cpp
+++ test/linux/delta_rs/delta_rs.cpp
@@ -6,10 +6,35 @@
 #include "arc_console.hpp"
 #include "arc_rt_tool.hpp"
 
+// ----------------------------------------------------------------
+// ----------------------------------------------------------------
+#define CPU_ID 7 // 指定運行的CPU編號
 
-// 全域變數表示是否收到訊號
+#define PERIOD_NS (1 * 1000 * 1000) // 1ms rt任務週期
+// ----------------------------------------------------------------
+
+// ----------------------------------------------------------------
+// realtime 時間測試
+int64_t max_dt = LLONG_MIN;
+int64_t min_dt = LLONG_MAX;
+int64_t sum_dt = 0;
+int64_t cyc_count = 0;
+void update_dt(int64_t dt)
+{
+    if (dt > max_dt)
+        max_dt = dt;
+
+    if (dt < min_dt)
+        min_dt = dt;
+
+    sum_dt += dt;
+    cyc_count++;
+}
+
+// ----------------------------------------------------------------
+// Ctrl+C 訊號
+// ----------------------------------------------------------------
 bool receivedCtrlC = false;
-// 自定義的信號處理函數
 void handleCtrlC(int sig)
 {
     console("Ctrl+C handle\n");
@@ -18,48 +43,106 @@ void handleCtrlC(int sig)
     receivedCtrlC = true;
 }
 
-void *threadFunc(void *arg)
+// ----------------------------------------------------------------
+// 主要的迴圈
+// ----------------------------------------------------------------
+void *bg_rt_dowork(void *arg)
 {
-    int tid = *(int *)arg;
-    printf("Thread %d: Hello, World!\n", tid);
-    while (!receivedCtrlC)
+    console("bg_rt_dowork thread start");
+
+    // 消除系統時間偏移的函數
+    if (set_latency_target())
     {
-        /* code */
+        console(RED "%s" RESET, strerror(errno));
     }
-    
+
+    // 指定cpu
+    setThreadAffinity(CPU_ID);
+
+    // config thread to realtime 
+    if (setThreadPriority(99) != 0 || setThreadNiceness(-20) != 0)
+    {
+        console("setThreadPriority or setThreadNiceness fail");
+    }
+    else
+    {
+        console("Starting RT task with dt=%u ns", PERIOD_NS);
+        
+        // cycletime in ns
+        const int64_t cycletime = PERIOD_NS; 
+
+        //  當前的精準時間
+        struct timespec wakeup_time;
+        if (clock_gettime(CLOCK_MONOTONIC, &wakeup_time) == -1) //f
+        {
+            console("clock_gettime " RED "%s" RESET, strerror(errno));
+            return NULL;
+        }
+
+        int64_t dt;
+        struct timespec tnow;
+
+        while (true)
+        {
+            // sleep直到指定的時間點
+            addTimespec(&wakeup_time, cycletime);
+            int ret = clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &wakeup_time, NULL);
+            if (ret)
+            {
+                // sleep錯誤處理
+                printf("clock_nanosleep(): %s\n", strerror(ret));
+                // break;
+            }
+
+            // 取得當前的精準時間
+            clock_gettime(CLOCK_MONOTONIC, &tnow);
+
+            // 計算時間差距
+            int64_t dt = calcDiff_ns(tnow, wakeup_time);
+            update_dt(dt);
+
+            // 顯示
+            EXEC_INTERVAL(30)
+            {
+                consoler("cyc_count: %ld, Latency:(act, min, max, avg)us = (%-8ld, %ld, %ld, %.2f) ****",
+                         cyc_count,
+                         dt / 1000,
+                         min_dt / 1000,
+                         max_dt / 1000,
+                         (double)sum_dt / cyc_count / 1000);
+            }
+            EXEC_INTERVAL_END
+
+            if (receivedCtrlC)
+                break;
+        }
+    }
+
+    console("bg_rt_dowork thread exit");
     return NULL;
 }
-
 
 int main()
 {
     console("delta_rs SOEM (Simple Open EtherCAT Master) Start...");
 
-    // 消除系統時間偏移的函數
-    if (set_latency_target())
-    {
-        console("[error]" RED "%s" RESET, strerror(errno));
-        return -1;
-    }
-
     // 攔截ctrl+c事件
     signal(SIGINT, handleCtrlC);
 
-    pthread_t thread;
-    int tid = 123;
-    int ret = pthread_create(&thread, NULL, threadFunc, &tid);
-    if (ret != 0) {
-        fprintf(stderr, "Failed to create thread\n");
-        return 1;
+    // 開始執行背景執行
+    pthread_t bg_rt;
+    if (pthread_create(&bg_rt, NULL, bg_rt_dowork, NULL) != 0)
+    {
+        fprintf(stderr, "Failed to create bg_rt thread\n");
+        return -1;
     }
 
     // 等待執行緒結束
-    ret = pthread_join(thread, NULL);
-    if (ret != 0) {
-        fprintf(stderr, "Failed to join thread\n");
-        return 1;
+    if (pthread_join(bg_rt, NULL) != 0)
+    {
+        fprintf(stderr, "Failed to join bg_rt thread\n");
+        return -1;
     }
-
 
     return 0;
 }
